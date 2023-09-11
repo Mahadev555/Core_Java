2824fcd40d3e1d79e28cf5b97b0756ad3799e30f
Mahadev555 <mahadevmanerikar@gmail.com>
2023-09-11 22:56:52 +0530

    Constructor this and super method  added

---
+++ OOP_pw_Java/Constructor/ThisSuper.java
@@ -0,0 +1,12 @@
+package OOP_pw_Java.Constructor;
+
+public class ThisSuper {
+    public static void main(String[] args) {
+
+        // super call constructer from parent class
+        // this call constructer from same class
+
+
+
+    }
+}
