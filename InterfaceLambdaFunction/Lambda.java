551584e4ceea34f8866a7303f31e3171730145bd
Mahadev555 <mahadevmanerikar@gmail.com>
2023-10-17 17:47:40 +0530

    Lambda FUnction  Added

--- InterfaceLambdaFunction/Lambda.java
+++ InterfaceLambdaFunction/Lambda.java
@@ -1,4 +1,12 @@
 package InterfaceLambdaFunction;
 
+interface D{
+    void drive();
+}
 public class Lambda {
+    public static void main(String[] args) {
+       // Lambda Experssion
+        D obj = () -> System.out.println("Drive");
+        obj.drive();
+    }
 }
