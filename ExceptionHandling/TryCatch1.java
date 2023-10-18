7d5f651d26ce1355c25a925554c5ea7204f05ebd
Mahadev555 <mahadevmanerikar@gmail.com>
2023-10-18 22:12:27 +0530

    try catch Added

--- ExceptionHandling/TryCatch1.java
+++ ExceptionHandling/TryCatch1.java
@@ -6,14 +6,22 @@ public class TryCatch1 {
     public static void main(String[] args) {
         int a = 6;
         int b = 0;
+        int arr[] = {2,3,4,5};
         int result = 0;
 
         try{
             result = a/b;
         }
-        catch (Exception e){
+        catch (ArithmeticException e){
             System.out.println("Erroe : "+e);
         }
+        catch (ArrayIndexOutOfBoundsException obj){
+            System.out.println(obj);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
         System.out.println(result);
     }
 }
